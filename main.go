a6d3a7c26a60f041d12c9c67f44589d1eb5167ad
Matthew Franchini <matchewf11@gmail.com>
2025-07-03 00:39:25 -0700

    Refactor main for better scaling

--- main.go
+++ main.go
@@ -8,43 +8,69 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
+// need to validate latex
+// need to validate before saving as json
+// need to validate before doing the sql
+
 const (
-	storageDir   = "storage"
-	imgDir       = "images"
-	jsonFileName = "data.json"
-	sqlFileName  = "data.sql"
-	port         = ":8080"
+	storageDir    = "storage"
+	imgDir        = "images"
+	latexImg      = "preview"
+	jsonFileName  = "data.json"
+	sqlFileName   = "data.sql"
+	plainFileName = "data.txt"
+	port          = ":8080"
+	jsonFormat    = `
+  {
+    "section": "%s",
+    "difficulty": "%s",
+    "problem": "%s"
+  },`
+	latexFormat = `\documentclass[preview]{standalone}
+\usepackage{amsmath}
+\begin{document}
+%s
+\end{document}`
+	sqlFormat = `
+INSERT INTO problems (course, difficulty, problem_text)
+VALUES (%s, %s, %s);`
+	plainFormat = "%s\n%s\n%s\n"
 )
 
 //go:embed index.html
 var htmlTemplate string
 
-var jsonFile, sqlFile *os.File
-
-// make a directory, will possible throw
-func makeDir(fldrName string) {
-	if err := os.MkdirAll(fldrName, 0755); err != nil {
-		log.Fatal(err)
-	}
-}
-
-// make a wr file, will possibly throw
-func makeFile(fileName string) *os.File {
-	filePath := fmt.Sprintf("%s/%s", storageDir, fileName)
-	file, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return file
-}
+var jsonFile, sqlFile, plainFile *os.File
 
 // Init the file vars
 func init() {
-	makeDir(storageDir)
-	makeDir(imgDir)
-	jsonFile, sqlFile = makeFile(jsonFileName), makeFile(sqlFileName)
+
+	dirToMake := []string{storageDir, imgDir}
+
+	for _, dir := range dirToMake {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	filesToMake := map[string]**os.File{
+		jsonFileName:  &jsonFile,
+		sqlFileName:   &sqlFile,
+		plainFileName: &plainFile,
+	}
+
+	for name, file := range filesToMake {
+		filePath := filepath.Join(storageDir, name)
+		temp, err := os.OpenFile(filePath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		*file = temp
+	}
+
 }
 
 // starting point of the program
@@ -53,6 +79,7 @@ func main() {
 	defer func() {
 		sqlFile.Close()
 		jsonFile.Close()
+		plainFile.Close()
 	}()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) { fmt.Fprint(w, htmlTemplate) })
@@ -61,7 +88,8 @@ func main() {
 
 	fmt.Printf("http://localhost%s/\n", port)
 
-	if err := http.ListenAndServe(port, nil); err != nil {
+	err := http.ListenAndServe(port, nil)
+	if err != nil {
 		log.Fatal(err)
 	}
 
@@ -70,10 +98,16 @@ func main() {
 // compile latex and display
 func previewHandler(w http.ResponseWriter, r *http.Request) {
 
-	type formInfo struct {
+	defer func() {
+		fileTypesToDelete := []string{".aux", ".log", ".pdf", ".tex"}
+		for _, fileType := range fileTypesToDelete {
+			os.Remove(latexImg + fileType)
+		}
+	}()
+
+	var currForm struct {
 		Problem string `json:"problem"`
 	}
-	var currForm formInfo
 
 	err := json.NewDecoder(r.Body).Decode(&currForm)
 	if err != nil || currForm.Problem == "" {
@@ -81,44 +115,34 @@ func previewHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	latex := fmt.Sprintf(`\documentclass[preview]{standalone}
-\usepackage{amsmath}
-\begin{document}
-%s
-\end{document}`, currForm.Problem)
-
-	err = os.WriteFile("preview.tex", []byte(latex), 0644)
+	latex := fmt.Sprintf(latexFormat, currForm.Problem)
+	err = os.WriteFile(latexImg+".tex", []byte(latex), 0644)
 	if err != nil {
-		fmt.Println(err)
 		http.Error(w, "unable to write the latex to file", http.StatusInternalServerError)
 		return
 	}
 
-	cmd := exec.Command("pdflatex", "-interaction=nonstopmode", "preview.tex")
-	cmd.Dir = "."
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	cmd := exec.Command("pdflatex", "-interaction=nonstopmode", latexImg+".tex")
+	cmd.Dir, cmd.Stdout, cmd.Stderr = ".", os.Stdout, os.Stderr
+
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println(err)
 		http.Error(w, "Error compiling LaTeX", http.StatusInternalServerError)
 		return
 	}
 
-	cmd = exec.Command("magick", "-density", "300", "preview.pdf", "-quality", "90", "images/preview.png")
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	pngPath := filepath.Join(imgDir, latexImg+".png")
+
+	cmd = exec.Command("magick", "-density", "300", latexImg+".pdf", "-quality", "90", pngPath)
+	cmd.Stdout, cmd.Stderr = os.Stdout, os.Stderr
+
 	err = cmd.Run()
 	if err != nil {
 		http.Error(w, "ImageMagick conversion failed", http.StatusInternalServerError)
 		return
 	}
 
-	for _, f := range []string{"preview.aux", "preview.log", "preview.pdf", "preview.tex"} {
-		os.Remove(f)
-	}
-
-	imgData, err := os.ReadFile("images/preview.png")
+	imgData, err := os.ReadFile(pngPath)
 	if err != nil {
 		http.Error(w, "Error reading image", http.StatusInternalServerError)
 		return
@@ -132,36 +156,30 @@ func previewHandler(w http.ResponseWriter, r *http.Request) {
 // save as both json and sql insert queries
 func submitHandler(w http.ResponseWriter, r *http.Request) {
 
-	type formInfo struct {
+	var currForm struct {
 		Problem    string `json:"problem"`
 		Section    string `json:"section"`
 		Difficulty string `json:"difficulty"`
 	}
 
-	var currForm formInfo
 	err := json.NewDecoder(r.Body).Decode(&currForm)
 	if err != nil || currForm.Problem == "" || currForm.Section == "" || currForm.Difficulty == "" {
 		http.Error(w, "Invalid JSON", http.StatusBadRequest)
 		return
 	}
 
-	_, err = fmt.Fprintf(jsonFile, `
-{
- "section": "%s",
- "difficulty": "%s",
-  "problem": "%s"
-},`, currForm.Section, currForm.Difficulty, currForm.Problem)
-	if err != nil {
-		http.Error(w, "Unable to write to files", http.StatusInternalServerError)
-		return
+	filesToWrite := map[string]*os.File{
+		jsonFormat:  jsonFile,
+		sqlFormat:   sqlFile,
+		plainFormat: plainFile,
 	}
 
-	_, err = fmt.Fprintf(sqlFile, `
-INSERT INTO problems (course, difficulty, text)
-VALUES (%s, %s, %s);`, currForm.Section, currForm.Difficulty, currForm.Problem)
-	if err != nil {
-		http.Error(w, "Unable to write to files", http.StatusInternalServerError)
-		return
+	for fileFormat, file := range filesToWrite {
+		_, err = fmt.Fprintf(file, fileFormat, currForm.Section, currForm.Difficulty, currForm.Problem)
+		if err != nil {
+			http.Error(w, "Unable to write to files", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	w.Header().Set("Content-Type", "application/json")
